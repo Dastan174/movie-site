aacb059cb43a22dd381cacf25ea8a98ded820637
Amirovvas <amirova.saida06@gmail.com>
2026-03-24 18:48:09 +0600

    project-structure

---
+++ src/components/layout/Layout.tsx
@@ -0,0 +1,6 @@
+import "./Layout.scss";
+const Layout = () => {
+  return <div></div>;
+};
+
+export default Layout;
---
+++ src/components/layout/header/Header.tsx
@@ -0,0 +1,6 @@
+import "./Header.scss";
+const Header = () => {
+  return <div></div>;
+};
+
+export default Header;
