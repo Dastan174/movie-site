0886bd04580535c17b6155830e0eca22e024b68d
myrza333 <akmatovmyrza000@gamil.com>
2026-03-26 17:35:22 +0600

    закончил hero

--- src/routes/MainRoutes.tsx
+++ src/routes/MainRoutes.tsx
@@ -1,8 +1,9 @@
 import { Route, Routes } from "react-router-dom";
+import Hero from "../components/pages/hero/Hero";
 
 const MainRoutes = () => {
   const routes = [
-    { link: "/", element: "" },
+    { link: "/", element: <Hero /> },
     { link: "/", element: "" },
   ];
   return (
@@ -15,3 +16,12 @@ const MainRoutes = () => {
 };
 
 export default MainRoutes;
+
+// 1. Задача алынды
+// 2. git pull origin master
+// 3. задача кылынат
+// 4. git switch -c ветка
+// 5. git add .
+// 6. git commit -m "task"
+// 7. git push origin ветка
+// 8. github, compare and pull request
